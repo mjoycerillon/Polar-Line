3d8ca1d82752709d9d877bb4296a119d3cd4520e
AnugnaPatibandla <anugnachowdary11@gmail.com>
2021-04-10 21:26:46 -0400

    update

--- scripts/polarline.js
+++ scripts/polarline.js
@@ -136,6 +136,25 @@ $(function() {
     /**
      * Register Button
      */
+     $('#registerForm').on('submit', function(event) {
+        accounts=localStorage.getItem("accounts");
+        var accountsObj=JSON.parse(accounts);
+        accountsObj=JSON.parse(accounts);
+        accountsObj.push(
+            {
+                "first_name": $('#txtRegisterFirstName').val(),
+                "last_name": $('#txtRegisterLastName').val(),
+                "email": $('#txtRegisterEmail').val(),
+                "password": $('#txtRegisterPassword').val(),
+                "billing_address": "",
+                "shipping_address": "",
+                "birth_date": "",
+                "cart": []
+            }
+        );
+        accounts=JSON.stringify(accountsObj);
+        localStorage.setItem("accounts",accounts);                     
+    });
 
 
 });
